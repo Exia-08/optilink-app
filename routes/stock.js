f8adab8f5685889850c36681201c0986921e7624
Exia-08 <johnmichaelmarangit@gmail.com>
2026-08-23 16:41:18 +0800

    Update stock.js

--- routes/stock.js
+++ routes/stock.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 // Get all stock
 router.get('/', async (req, res) => {
-    const stock = await StockItem.find();
+    const { clinicId } = req.query;
+    const filter = clinicId ? { clinicId } : {};
+    const stock = await StockItem.find(filter).sort({ name: 1 });
     res.json({ stock });
 });
 
